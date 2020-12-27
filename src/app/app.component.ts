d2a009f275a2661ea3e224e749ee5bbb7eeafeff
unknown <meieramberj@gmail.com>
2020-12-27 16:51:01 -0600

    Re-factored Part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'orbit-report';
-  sourceList: Satellite[];
-  constructor() {
+
+export class List{
+sourceList: Satellite[];
+constructor() {
     this.sourceList = [
       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
@@ -18,6 +13,17 @@ export class AppComponent {
       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
     ];
   }
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'orbit-report';
+  
 }
 
 
