f5a962b7e52e8c0656b106b3ee5d28700176f917
unknown <meieramberj@gmail.com>
2020-12-28 16:05:18 -0600

    Completed parts 6-8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
    window.fetch(satellitesUrl).then(function(response) {
@@ -22,10 +24,23 @@ export class AppComponent {
           this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational));
          }   
          console.log(this.sourceList);        
-         
+         this.displayList = this.sourceList.slice(0);
       }.bind(this));
    }.bind(this));
+   
  }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  this.displayList = matchingSatellites;
+}
 }
 
 
